b9d527f9b59fec01c432258ebf8c7743ca2d422e
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2024-12-20 23:41:12 +0530

    adding more problems
    adding sum of first n natural numbers solution in java script

---
+++ JavaScript/Beginner/sumOfNumbers.js
@@ -0,0 +1,9 @@
+// This program calculates the sum of the first n natural numbers
+let n = 5;  // Change the value of n
+let sum = 0;
+
+for (let i = 1; i <= n; i++) {
+    sum += i;
+}
+
+console.log("Sum of first " + n + " natural numbers is: " + sum);
