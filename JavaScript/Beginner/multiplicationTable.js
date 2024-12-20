2323a22e57801dd114384298dacfd9a07283373a
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2024-12-20 23:43:22 +0530

    adding table.js
    for printing table just hit the run button

---
+++ JavaScript/Beginner/multiplicationTable.js
@@ -0,0 +1,6 @@
+// This program prints the multiplication table for a given number
+let number = 5;  // Change the number here
+
+for (let i = 1; i <= 10; i++) {
+    console.log(number + " x " + i + " = " + (number * i));
+}
