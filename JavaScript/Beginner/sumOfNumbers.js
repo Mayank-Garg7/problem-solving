fd97d0a036b00df8cfa726233c6e8eb5449bdbbe
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2024-12-21 19:56:14 +0530

    Update sumOfNumbers.js
    taking input from the user

--- JavaScript/Beginner/sumOfNumbers.js
+++ JavaScript/Beginner/sumOfNumbers.js
@@ -1,9 +1,10 @@
 // This program calculates the sum of the first n natural numbers
-let n = 5;  // Change the value of n
-let sum = 0;
+let str = prompt("Enter a string to check if it's a palindrome:");
+let reversedStr = str.split('').reverse().join('');
 
-for (let i = 1; i <= n; i++) {
-    sum += i;
+if (str.toLowerCase() === reversedStr.toLowerCase()) { // Case-insensitive comparison
+    console.log(str + " is a palindrome");
+} else {
+    console.log(str + " is not a palindrome");
 }
 
-console.log("Sum of first " + n + " natural numbers is: " + sum);
