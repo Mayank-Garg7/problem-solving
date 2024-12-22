7b69d0022e0b068bbb077a72918a18878c46d6e2
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2024-12-22 20:31:11 +0530

    reading the whole line of input for further process

--- C-programming/helloWorld.c
+++ C-programming/helloWorld.c
@@ -4,9 +4,9 @@ int main() {
     char s[100]; // Allocate memory for the string with a sufficient size
     printf("Hello World!\n");
 
-    printf("Enter your name: ");
-    scanf("%99s", s); // Use %s to read a string and limit the input to prevent buffer overflow
+    printf("Enter your full name: ");
+    fgets(s, sizeof(s), stdin); // Read the entire line, including spaces
 
-    printf("Your name is: %s\n", s); // Use %s to display the string
+    printf("Your name is: %s", s);
     return 0;
 }
