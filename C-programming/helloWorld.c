6b9eaa9d7ecf99d5ac541599fe6f8a6ad3e2cc81
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2024-12-22 20:26:52 +0530

    updating the helloWorld.c file for taking input as well

--- C-programming/helloWorld.c
+++ C-programming/helloWorld.c
@@ -1,4 +1,12 @@
-#include<stdio.h>
-int main(){
-    printf("Hello World!");
+#include <stdio.h>
+
+int main() {
+    char s[100]; // Allocate memory for the string with a sufficient size
+    printf("Hello World!\n");
+
+    printf("Enter your name: ");
+    scanf("%99s", s); // Use %s to read a string and limit the input to prevent buffer overflow
+
+    printf("Your name is: %s\n", s); // Use %s to display the string
+    return 0;
 }
