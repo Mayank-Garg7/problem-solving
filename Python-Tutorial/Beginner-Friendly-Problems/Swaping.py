8657c15ad59588326d4e7e631f1ead9b58727422
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2024-12-23 20:49:33 +0530

    adding more approaches for solving the same problem

--- Python-Tutorial/Beginner-Friendly-Problems/Swaping.py
+++ Python-Tutorial/Beginner-Friendly-Problems/Swaping.py
@@ -1,5 +1,20 @@
 a = int(input("enter the first number : "))
 b = int(input("enter the second number : "))
 print("number before swaping : ",a," ",b)
-a,b = b,a
+
+# Approach-1
+a = a+b
+b = a-b
+a = a-b
+print("number after swaping : ",a," ",b)
+
+# Approach-2
+a, b = b, a
 print("number after swapping : ",a," ",b)
+
+# Approach-3
+c = a
+a = b
+b = a 
+print("number after swapping : ",a," ",b)
+
