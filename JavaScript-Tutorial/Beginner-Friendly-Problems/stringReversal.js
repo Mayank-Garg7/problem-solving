da605933ec6487874d3a7f24a193222260d5c8da
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2024-12-23 20:22:28 +0530

    adding string reversal here

---
+++ JavaScript-Tutorial/Beginner-Friendly-Problems/stringReversal.js
@@ -0,0 +1,5 @@
+// This program reverses a string
+let str = "hello";  // Change the string here
+let reversedStr = str.split('').reverse().join('');
+
+console.log("Reversed string is: " + reversedStr);
