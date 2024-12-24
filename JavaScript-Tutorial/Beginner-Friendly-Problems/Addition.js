65f23b444491809b1dbc34fee7e7a39500730880
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2024-12-24 22:02:33 +0530

    adding subraction operator in javascript

--- JavaScript-Tutorial/Beginner-Friendly-Problems/Addition.js
+++ JavaScript-Tutorial/Beginner-Friendly-Problems/Addition.js
@@ -1,5 +1,13 @@
 // this program for adding two numbers input by the user
 let num1 = parseInt(prompt("Enter the first number : "))
 let num2 = parseInt(prompt("Enter the second number : "))
-let sum = num1 + num2
+// Arithmatic Operators
+// 1. Addition : num1 + num2
+let sum = num1 + num2;
 console.log("Addition of two given numbers is : "+sum);
+
+// 2. Subtraction : num1 - num2
+let difference = num1 - num2;
+console.log("Subtraction of two given numbers is : "+difference);
+
+
