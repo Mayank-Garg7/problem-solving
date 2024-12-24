327790c81c65888e3b098e4eea7e47d2e696f7d6
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2024-12-24 21:58:16 +0530

    adding basic of arithmetic operator

---
+++ JavaScript-Tutorial/Beginner-Friendly-Problems/Addition.js
@@ -0,0 +1,5 @@
+// this program for adding two numbers input by the user
+let num1 = parseInt(prompt("Enter the first number : "))
+let num2 = parseInt(prompt("Enter the second number : "))
+let sum = num1 + num2
+console.log("Addition of two given numbers is : "+sum);
