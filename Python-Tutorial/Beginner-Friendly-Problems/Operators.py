e0cb9872f751651f7eb0d8aeaa39fa5c64e868a5
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2024-12-25 17:56:28 +0530

    removing extra comments from code

--- Python-Tutorial/Beginner-Friendly-Problems/Operators.py
+++ Python-Tutorial/Beginner-Friendly-Problems/Operators.py
@@ -156,10 +156,10 @@ print("not False:", not False)
 # ---------------------------------------------------------------------------------------------------------------------------------
 # Membership Operators
 my_list = [1, 2, 3, 4, 5]
-print(3 in my_list)  # Output: True
-print(6 in my_list)  # Output: False
-print(3 not in my_list)  # Output: False
-print(6 not in my_list)  # Output: True
+print(3 in my_list)  
+print(6 in my_list)  
+print(3 not in my_list)  
+print(6 not in my_list)  
 
 
 # ---------------------------------------------------------------------------------------------------------------------------------
